9aaa9f14bc00b69b3051b60d24ff035c47fb1ecf
Cai41 <caiyang0226@outlook.com>
2016-09-18 16:51:12 -0400

    update

--- p1/client.py
+++ p1/client.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument('neuid')
     args = parser.parse_args()
     # print args
+    
     port = 27993
     if args.port is not None:
         port = int(args.port)
@@ -17,10 +18,17 @@ def main():
         port = 27994
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket = args.ssl and ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_SSLv23) or sock
-    clientSocket.connect((args.hostname, port))
+    try:
+        clientSocket.connect((args.hostname, port))
+    except Exception as e:
+        print 'can not connect to %s:%d, err:%s' % (args.hostname, port, e)
+        clientSocket.close()
+        return
+    
     helloMsg = 'cs5700fall2016 HELLO ' + args.neuid + '\n'
     clientSocket.send(helloMsg)
     # print helloMsg
+    
     while True:
         statusMsg = clientSocket.recv(256)
         # print statusMsg
@@ -28,10 +36,16 @@ def main():
             statusPos = statusMsg.find('STATUS')
             replyMsg = "cs5700fall2016 " +  str(eval(statusMsg[statusPos+7:-1])) + "\n"
             clientSocket.send(replyMsg)
-        else:
-            clientSocket.close()
-            print statusMsg
+        elif 'BYE' in statusMsg:
+            # print statusMsg
+            pos1 = statusMsg.find('fall2016')
+            pos2 = statusMsg.find('BYE')
+            print statusMsg[pos1+9:pos2-1]
             break
+        else:
+            print "Unknow message"
+            break
+    clientSocket.close()
 
 if __name__=='__main__':
     main()
