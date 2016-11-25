6095f8e87bbae59268aca5d643f96c90fdb94d43
Cai41 <caiyang0226@outlook.com>
2016-11-25 04:05:27 -0500

    basic dns always return origin ip

--- p5/dnsserver.py
+++ p5/dnsserver.py
@@ -1,6 +1,7 @@
 import SocketServer
 import argparse
 import socket
+import struct
 
 """
 DNS Message Header:
@@ -16,19 +17,63 @@ DNS Message Header:
 
 """
 
-hosts = {'ec2-54-167-4-20.compute-1.amazonaws.com':None,
-         'ec2-54-210-1-206.compute-1.amazonaws.com':None}
+hosts = [('ec2-54-167-4-20.compute-1.amazonaws.com',None),
+         ('ec2-54-210-1-206.compute-1.amazonaws.com',None)]
 
-dns_fmt = {'id', 'flags', 'qscount', 'arcount', 'nscount', 'arcount'}
+dns_fmt = ['id', 'flags', 'qscount', 'ancount', 'nscount', 'adcount']
+record_fmt = ['name', 'type', 'class', 'ttl', 'len', 'rdata']
 
-for h in hosts:
-    hosts[h] = socket.gethostbyname(h)
+for i in range(len(hosts)):
+    hosts[i] = (hosts[i][0], socket.inet_aton(socket.gethostbyname(hosts[i][0])))
     
 class DNSHandler(SocketServer.BaseRequestHandler):
     def handle(self):
-        data = self.request[0].strip()
-        sock = self.request[1]        
-        sock.sendto(data, self.client_address[0]) 
+        dnsHeader, data = self._stripDNSHeader(self.request[0])
+        sock = self.request[1]
+
+        query, restData = self._extractQuery(data)
+
+        ansHdr = self._hdrDict(dnsHeader['id'])
+        recDict = self._recDict(self._encodeName('cs5700cdnproject.ccs.neu.edu'), hosts[0][1])
+        ansHdr['flags'] = 1 << 15
+
+        sendData = self._buildHdr(ansHdr) + query + self._buildRec(recDict)
+        # print ':'.join(x.encode('hex') for x in sendData)
+        sock.sendto(sendData, self.client_address)
+
+    def _stripDNSHeader(self, data):
+        header = struct.unpack('!HHHHHH', data[:12])
+        header_dict = dict(zip(dns_fmt, header))
+        print header_dict
+        return header_dict, data[12:]
+
+    def _hdrDict(self, id):
+        return {'id':id,'flags':0,'qscount':1,'ancount':1,'nscount':0,'adcount':0}
+
+    def _buildHdr(self, hdrDict):
+        return struct.pack('!HHHHHH', hdrDict['id'], hdrDict['flags'],
+                    hdrDict['qscount'], hdrDict['ancount'], hdrDict['nscount'], hdrDict['adcount'])
+
+    def _recDict(self, name, ip):
+        return {'name':name, 'type':1, 'class':1, 'ttl':100,'len':4,'rdata':ip}
+
+    def _buildRec(self, rec):
+        print rec
+        return rec['name'] + struct.pack('!HHIH', rec['type'],rec['class'],rec['ttl'],rec['len']) + rec['rdata']
+
+    def _extractQuery(self, data):
+        pos = data.find('\x00')
+        pos += 5
+        return data[:pos], data[pos:]
+
+    def _encodeName(self, s):
+        labels = s.split('.')
+        res = ''
+        for l in labels:
+            res += struct.pack('B', len(l))
+            res += l
+        res += '\x00'
+        return res
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="client")
