dd4518b8045b3c34da7ff5f0c9e2bac1ef903122
Cai41 <caiyang0226@outlook.com>
2016-11-28 16:44:34 -0500

    lru

---
+++ p5/LRUCache.py
@@ -0,0 +1,104 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev, self.next = None, None
+
+class DoubleList:
+    # initialize with dummy head and tail
+    def __init__(self):
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+
+    # insert after head
+    def add(self, n):
+        return self.insert(n, self.head, self.head.next)
+
+    # insert a node n between prev and next
+    def insert(self, n, prev, next):
+        n.next = next
+        n.prev = prev
+        prev.next = n
+        next.prev = n
+        self.size += 1
+        return n
+
+    # remove last node
+    def removeLast(self):
+        return self.remove(self.tail.prev)
+
+    # remove node n
+    def remove(self, n):
+        if n is self.head or n is self.tail:
+            return None
+        n.prev.next = n.next
+        n.next.prev = n.prev
+        self.size -= 1
+        return n
+
+class Cache:
+    def __init__(self, capacity):
+        self.cap = capacity
+        self.items = DoubleList()
+        self.mappings = {}
+        self.size = 0
+
+    def get(self, key):
+        if key not in self.mappings: return None
+        n = self.mappings[key]
+        n = self.items.remove(n)
+        self.items.add(n)
+        return n.value
+
+    def insert(self, key, value):
+        # if val's size is too large, return false because we can't cache it
+        if len(value) >= self.cap:
+            return False
+
+        if key in self.mappings:
+            n = self.items.remove(self.mappings[key])
+            self.size = self.size - len(n.value) + len(value)
+            n.value = value
+        else:
+            self.size += len(value)
+            self.mappings[key] = Node(key, value)
+
+        n = self.mappings[key]
+        self.items.add(n)
+        # while total size is too large, keep deleting lru item from cache            
+        while self.size >= self.cap:
+            n = self.items.removeLast()
+            self.size -= len(n.value)
+            del self.mappings[n.key]            
+
+        return True
+
+    def print_info(self):
+        n = self.items.head.next
+        while n.key is not None:
+            print '[' + str(n.key) +', ' + str(n.value) + '], ',
+            n = n.next
+        print 'number of keys: ' + str(len(self.mappings)) + ', ',
+        print 'total size: ' + str(self.size)
+    
+if __name__ == '__main__':
+    cache = Cache(5)
+    cache.insert(1,'x')
+    cache.insert(2,'y')
+    cache.print_info()
+    cache.get(1)
+    cache.print_info()
+    cache.get(2)
+    cache.get(2)    
+    cache.print_info()
+    cache.insert(3,'z')
+    cache.insert(4,'a')
+    cache.print_info()        
+    cache.insert(5,'abc')
+    cache.print_info()
+    cache.insert(6,'k')
+    cache.print_info()    
+    
