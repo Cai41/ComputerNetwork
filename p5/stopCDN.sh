8378671f05a3589d71e05b5c8288de5c2df08687
Cai41 <caiyang0226@outlook.com>
2016-12-09 01:59:30 -0500

    stop cdn

---
+++ p5/stopCDN.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+hosts=(ec2-54-210-1-206.compute-1.amazonaws.com
+       ec2-54-67-25-76.us-west-1.compute.amazonaws.com
+       ec2-35-161-203-105.us-west-2.compute.amazonaws.com
+       ec2-52-213-13-179.eu-west-1.compute.amazonaws.com
+       ec2-52-196-161-198.ap-northeast-1.compute.amazonaws.com
+       ec2-54-255-148-115.ap-southeast-1.compute.amazonaws.com
+       ec2-13-54-30-86.ap-southeast-2.compute.amazonaws.com
+       ec2-52-67-177-90.sa-east-1.compute.amazonaws.com
+       ec2-35-156-54-135.eu-central-1.compute.amazonaws.com
+      )
+
+while [[ $# -gt 1 ]]
+do
+    key="$1"
+    case $key in
+	-p)
+	    PORT="$2"
+	    shift
+	    ;;
+	-o)
+	    ORIGIN="$2"
+	    shift
+	    ;;
+	-n)
+	    NAME="$2"
+	    shift
+	    ;;
+	-u)
+	    USERNAME="$2"
+	    shift
+	    ;;
+	-i)
+	    KEYFILE="$2"
+	    shift
+	    ;;
+    esac
+shift 
+done
+
+for h in "${hosts[@]}"
+do
+    # note: must use double quote here
+    ssh -i $KEYFILE $USERNAME@$h "killall python > /dev/null 2>&1 &"
+done
+
+ssh -i $KEYFILE $USERNAME@cs5700cdnproject.ccs.neu.edu "killall python > /dev/null 2>&1 &"
